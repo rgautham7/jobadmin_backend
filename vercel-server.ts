224dd54933e302ad7ce5266af432f1705a3827a4
rgautham7 <gauthamramesh98270@gmail.com>
2025-07-10 02:57:39 +0530

    Setup NestJS for Vercel deployment

--- vercel-server.ts
+++ vercel-server.ts
@@ -1,11 +1,12 @@
+// vercel-server.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 
 const server = express();
 
-const bootstrap = async (): Promise<void> => {
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   await app.init();
 };
