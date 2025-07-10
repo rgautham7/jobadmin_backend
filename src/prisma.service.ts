8b467051ff2f668a449e53d10dd5241ba90bef85
rgautham7 <gauthamramesh98270@gmail.com>
2025-07-10 08:55:56 +0530

    Add prisma postinstall and generate client

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,15 +1,18 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+// prisma.service.ts (Safe for serverless)
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+  log: ['error'],
+});
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  async onModuleInit() {
-    await this.$connect();
-  }
-  async onModuleDestroy() {
-    await this.$disconnect();
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super();
   }
 }
