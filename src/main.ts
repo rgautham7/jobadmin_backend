1b15084e12e42302347250e9e19a398725cc74ca
rgautham7 <gauthamramesh98270@gmail.com>
2025-07-10 10:12:18 +0530

    Changes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'https://jobadmin-frontend.vercel.app',
+    origin: process.env.CORS_ORIGIN || 'https://jobadmin-frontend-rjb5b4y8l-gauthams-projects-94597e21.vercel.app',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
--- vercel-server.ts
+++ vercel-server.ts
@@ -9,7 +9,7 @@ const server = express();
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'https://jobadmin-frontend.vercel.app',
+    origin: process.env.CORS_ORIGIN || 'https://jobadmin-frontend-rjb5b4y8l-gauthams-projects-94597e21.vercel.app',
     credentials: true,
   });
   await app.init();
